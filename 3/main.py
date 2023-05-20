f647bf16402eb4a26109a575d93ae6e632866f1b
Khussein Reber <73395161+hmanilow@users.noreply.github.com>
2023-05-20 12:13:16 +0300

    Update main.py

--- 3/main.py
+++ 3/main.py
@@ -1,8 +1,17 @@
 while True:
 
     n = input("Введите строку: ")
-    mas=[]
-    fl=[]
+    mas = []
+    fl = []
     if len(n) == 0:
         print("нет")
     else:
+        if n.count('a')+n.count('b') == len(n):
+            flag = 0
+            s =''
+            r = ''
+            for i in n:
+                s += i
+                flag = func(i, flag)
+                mas.append(s)
+                fl.append(flag)
